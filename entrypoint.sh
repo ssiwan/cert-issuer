32de833120fd944859e1903d91c405aa0e1fd8c4
ssiwan <convicx@gmail.com>
2018-09-10 15:53:11 -0700

    Blah.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-FILE="~/.bitcoin/testnet3/.lock"
+FILE="/.bitcoin/testnet3/.lock"
+
+ls -la /.bitcoin/testnet3
 
 if [ -f $FILE ]; then
+    echo "Found lock. Deleting.."
     rm $FILE
+else
+    echo "Did not find lock."
 fi
+
 
 exec "$@"
